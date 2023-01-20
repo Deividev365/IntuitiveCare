4b5320ba7f73c2f928fc56621978ebfae956e716
Deivid Almeida <deividalmeida365@gmail.com>
2023-01-20 14:52:55 -0300

    test02/feature/transformFile/converted .pdf file into .zip

--- transformData/transformData.py
+++ transformData/transformData.py
@@ -1,20 +1,30 @@
 import tabula
 from zipfile import ZipFile, ZIP_DEFLATED
-
 ## acessar anexo do pdf baixado
 
 
-zip = ZipFile("../files/deivid.zip", "r")
 
+zip = ZipFile('files/deivid.zip', 'r')
 zip.extract("Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536_RN537_RN538_RN539_RN541_RN542_RN544_546.pdf")
 zip.close()
 
+
 pdf_path = "Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536_RN537_RN538_RN539_RN541_RN542_RN544_546.pdf"
 
-tabula.convert_into(pdf_path, "deivid.pdf", output_format="csv", pages="all")
+
+tabula.convert_into(pdf_path, "deivid.csv", output_format="csv", pages="all", stream=True)
 
 
+csv = open("deivid.csv", mode="r+")
+linhas = csv.readlines()
 
-csv = open("deivid.csv")
+for linha in linhas:
+    linha.replace("OD", "Seg. Odontológica")
+    linha.replace("AMB", "Seg. Ambulatorial")
+csv.writelines(linhas)
+csv.close()
 
-lerLinhas = csv.readlines()
+
+zip = ZipFile("deividv2.zip", "w", compression=ZIP_DEFLATED)
+zip.write("deivid.csv")
+zip.close()
