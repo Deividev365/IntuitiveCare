ed2a9e58cbc1d32a2fcdf25d6c3db8a150d10af7
Deivid Almeida <deividalmeida365@gmail.com>
2023-01-20 13:34:49 -0300

    test02/feature/transformFile/correction module zipFile

--- transformData/transformData.py
+++ transformData/transformData.py
@@ -11,17 +11,10 @@ zip.close()
 
 pdf_path = "Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536_RN537_RN538_RN539_RN541_RN542_RN544_546.pdf"
 
-tabula.convert_into(pdf_path, "deivid.csv", output_format="csv", pages="all")
+tabula.convert_into(pdf_path, "deivid.pdf", output_format="csv", pages="all")
+
 
 
 csv = open("deivid.csv")
 
 lerLinhas = csv.readlines()
-
-for lerLinha in lerLinhas:
-    lerLinha.replace("OD", "Seg. Odontológica")
-    lerLinha.replace("AMB", "Seg. Ambulatorial")
-
-
-csv.writelines(lerLinhas)
-csv.close()
